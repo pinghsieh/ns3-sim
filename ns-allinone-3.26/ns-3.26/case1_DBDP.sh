eca8ee9edd90b309e0eee7e8d9ab6cdb8a6595db
pinghsieh <lleyfede@tamu.edu>
2018-02-05 03:18:58 -0600

    Start collecting results using shell scripts

---
+++ ns-allinone-3.26/ns-3.26/case1_DBDP.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+time ./waf --run "scratch/RT-decentralized --nIntervals=3000 --testId=1 --alpha=0.6 --policy=DBDP"
+#time ./waf --run "scratch/RT-decentralized --nIntervals=3000 --testId=1 --alpha=0.58 --policy=DBDP"
+#time ./waf --run "scratch/RT-decentralized --nIntervals=3000 --testId=1 --alpha=0.56 --policy=DBDP"
+#time ./waf --run "scratch/RT-decentralized --nIntervals=3000 --testId=1 --alpha=0.54 --policy=DBDP"
+
